4270e3838e6401a362ceda6ce7b941685a5c176f
John Parris <public@johnparris.com>
2016-12-19 10:45:43 -0500

    WIP: nav menu restrictions. #202

--- includes/class-rcp-nav-menus.php
+++ includes/class-rcp-nav-menus.php
@@ -1,10 +1,14 @@
 <?php
+/**
+ * Note to developers: this is a temporary solution until https://core.trac.wordpress.org/ticket/18584 lands in core.
+ * Do not rely on this class to exist in this same format in future versions of RCP.
+ */
 
-class RCP_Nav_Menus {
+final class RCP_Nav_Menus {
 
-	public $member;
-	public $subscription_levels;
-	protected $menu_restrictions;
+	private $member;
+	private $subscription_levels;
+	private $member_subscription_id;
 
 	public function __construct() {
 
@@ -18,84 +22,41 @@ class RCP_Nav_Menus {
 	}
 
 	public function admin_init() {
-		add_action( 'admin_footer', array( $this, 'admin_scripts' ) );
-		add_action( 'save_post_nav_menu_item', array( $this, 'save_post_nav_menu_item'), 10, 3 );
+		add_filter( 'wp_edit_nav_menu_walker', array( $this, 'override_walker_nav_menu_edit' ) );
+		add_action( 'wp_update_nav_menu_item', array( $this, 'save_menu_item_restrictions' ), 10, 3 );
+	}
+
+	public function override_walker_nav_menu_edit() {
+		return 'RCP_Walker_Nav_Menu_Edit';
 	}
 
 	public function init() {
 
-		$this->menu_restrictions = $this->get_menu_restrictions();
-
 		$this->member = new RCP_Member( get_current_user_id() );
+		$this->member_subscription_id = $this->member->get_subscription_id();
 
 		add_filter( 'wp_nav_menu_objects', array( $this, 'filter_nav_menu_objects' ), 10, 2 );
 	}
 
-	public function admin_scripts() {
-
-		if ( 'nav-menus' !== get_current_screen()->id  || empty( $this->subscription_levels ) ) {
-			return;
-		}
-
-		wp_enqueue_script( 'underscore' );
-
-?>
-		<script type="text/javascript">
-
-			jQuery(document).ready(function($) {
-
-				var html = '';
-				var levels = {};
-				var description = '<?php _e( "Select the subscription levels that can view this menu item.", "rcp" ); ?>';
-				// nonce
-				$('<input type="hidden" name="rcp-nav-menu-nonce" value="<?php echo wp_create_nonce( "rcp-nav-menu-nonce" ); ?>" />').insertAfter('#menu-to-edit');
-<?php
-
-				foreach( $this->subscription_levels as $level ) :
-					$level_id = absint( $level->id );
-?>
-					levels[<?php echo $level_id; ?>] = '<?php echo esc_html( $level->name ); ?>';
-<?php
-				endforeach;
-?>
-
-				$('#menu-to-edit .menu-item').each(function() {
-
-					var id = $(this).attr( 'id' ), fields = '';
-
-					var menu_item_id = id.split('-').pop();
-
-					var html = build_inputs(menu_item_id);
-
-					fields = '<p>' + description + '</p>';
-					fields = fields + '<p>' + html + '</p>';
-
-					$(this).find('.menu-item-actions').prepend( fields );
-				});
-
-				function build_inputs(menu_item_id) {
-					var html = '';
-					_.each( levels, function(value, key) {
-						<?php
-						// $saved_levels = get_post_meta( $key, 'rcp_nav_menu_item_levels_required' ); ?>
-						html = html + '<label><input type="checkbox" id="rcp-nav-menu-item-'+key+'" name="rcp-nav-menu-item['+menu_item_id+']['+key+']" value="1" />'+value+'</label><br>';
-					});
-					return html;
-				}
-			});
-		</script>
-<?php
-	}
-
-	protected function get_menu_restrictions() {
-		global $wpdb;
-		return $wpdb->get_results( "SELECT post_id, meta_value FROM {$wpdb->postmeta} WHERE meta_key = 'rcp_nav_menu_item_levels_required'", ARRAY_A );
-	}
-
+	/**
+	 * Removes nav menu items from the menu if the user doesn't have access.
+	 */
 	public function filter_nav_menu_objects( $menu_items, $args ) {
 
+		if ( current_user_can( 'manage_options' ) ) {
+			return $menu_items;
+		}
+
 		foreach( $menu_items as $key => $item ) {
-			if( ! $this->should_show( $item->object_id ) ) {
+
+			$saved_levels = (array) get_post_meta( $item->ID, 'rcp_nav_menu_item_levels_required', true );
+
+			if ( ! empty( $saved_levels[0] ) && ! in_array( $this->member_subscription_id, $saved_levels ) ) {
+				unset( $menu_items[ $key ] );
+				continue;
+			}
+
+			if( ! $this->member->can_access( $item->object_id ) ) {
 				unset( $menu_items[ $key ] );
 			}
 		}
@@ -103,33 +64,60 @@ class RCP_Nav_Menus {
 		return $menu_items;
 	}
 
-	public function should_show( $object_id ) {
-		return $this->member->can_access( $object_id );
-	}
-
-	public function save_post_nav_menu_item( $post_id, $post, $update ) {
-
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			return;
-		}
+	/**
+	 * Saves the nav menu item restrictions.
+	 */
+	public function save_menu_item_restrictions( $menu_id, $menu_item_db_id, $args ) {
 
 		if ( empty( $_POST['rcp-nav-menu-nonce'] ) || ! wp_verify_nonce( $_POST['rcp-nav-menu-nonce'], 'rcp-nav-menu-nonce' ) ) {
 			return;
 		}
 
-		if ( empty( $_POST['rcp-nav-menu-item'] ) ) {
-			// @todo delete existing configs
+		if ( empty( $_POST['rcp-nav-menu-item'][$menu_item_db_id] ) ) {
+			delete_post_meta( $menu_item_db_id, 'rcp_nav_menu_item_levels_required' );
 			return;
 		}
 
-		foreach( $_POST['rcp-nav-menu-item'] as $key => $menu_item ) {
-			$levels = array_keys( array_map( 'absint', $menu_item ) );
-			update_post_meta( $key, 'rcp_nav_menu_item_levels_required', $levels );
-		}
+		$levels = array_keys( array_map( 'absint', $_POST['rcp-nav-menu-item'][$menu_item_db_id] ) );
+
+		update_post_meta( $menu_item_db_id, 'rcp_nav_menu_item_levels_required', $levels );
+	}
+
+	public function get_subscription_levels() {
+		return $this->subscription_levels;
 	}
 }
 
+/**
+ * Loads the RCP_Nav_Menus class.
+ *
+ * @since 2.7
+ */
 function rcp_setup_nav_menus() {
+
+	if ( version_compare( get_bloginfo( 'version' ), 4.7, '<' ) ) {
+		return;
+	}
+
+	global $rcp_nav_menus;
 	$rcp_nav_menus = new RCP_Nav_Menus;
 }
 add_action( 'plugins_loaded', 'rcp_setup_nav_menus', 100 );
+
+/**
+ * Loads the RCP_Walker_Nav_Menu_Edit class to add
+ * custom controls to the menu edit screen.
+ *
+ * @since 2.7
+ */
+function rcp_load_walker_nav_menu_edit() {
+
+	if ( version_compare( get_bloginfo( 'version' ), 4.7, '<' ) ) {
+		return;
+	}
+
+	if ( is_admin() ) {
+		require_once RCP_PLUGIN_DIR . 'includes/class-rcp-walker-nav-menu-edit.php';
+	}
+}
+add_action( 'admin_init', 'rcp_load_walker_nav_menu_edit' );
